aa14c9f72e3cb3bc11e1f921cc725ace74a7a987
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-29 14:40:41 -0400

    hero component which will render the hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className=" pb-20 pt-36">
+      <div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
+          officia dolor, officiis corporis nihil praesentium ab nisi. Error
+          expedita quas modi? Nulla in reprehenderit laudantium mollitia,
+          repellat facere aut iure!
+        </p>
+        <h1>Hello, portfolio</h1>
+        <p>Hey there i am this and this</p>
+        <button>Show my work</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
