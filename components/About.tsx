b42d021e65ab749459d98b7b9905044614549998
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-29 19:02:25 -0400

    initilizing the about section

---
+++ components/About.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <section className=" text-4xl  min-h-screen mt-20   bg-white/10 py-20 backdrop-blur">
+      <div className="flex">
+        <h1>About</h1>
+      </div>
+    </section>
+  );
+};
+
+export default About;
