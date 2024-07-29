3cb6440a50a0e39e8abceeffc368825cc09d8971
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-29 19:31:32 -0400

    rendering the navbar on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-hidden">
       <div className="max-w-7xl w-full">
+        <Navbar />
         {/* $ */}
         <Hero />
         {/* $ */}
