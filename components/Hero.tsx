5f503ac696afe7dc1d283983d46806b767a9e7ed
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-29 19:04:00 -0400

    adding acetrinity button and fixing the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import { Spotlight } from "./ui/Spotlight";
 import { FlipWords } from "./ui/flip-words";
+import { HoverBorderGradient } from "./ui/hover-border-gradient";
 
 const Hero = () => {
   const words = [
@@ -12,18 +13,15 @@ const Hero = () => {
   ];
 
   return (
-    <div className="pb-20 pt-36">
+    <section className="pb-20 pt-36">
       <div>
         <Spotlight className=" -top-40 -left-10 md:-left-32 md:-top-20 h-screen" />
-        <Spotlight
-          className="top-10 left-full h-[80vh] w-[50vw]"
-          fill="purple"
-        />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+        <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" />
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.024] bg-grid-black-100/[0.2] absolute top-0 left-0 flex items-center justify-center">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.04] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_4%,black)]"></div>
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
@@ -45,11 +43,12 @@ const Hero = () => {
             officia adipisci temporibus!
           </p>
 
-          <a href="#about"></a>
+          <a href="#about" className="mt-10">
+            <HoverBorderGradient>Learn More</HoverBorderGradient>
+          </a>
         </div>
       </div>
-      {/*  */}
-    </div>
+    </section>
   );
 };
 
