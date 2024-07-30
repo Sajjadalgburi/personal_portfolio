0e7eedced0cedd058ad792b0a52b25fc75d9c6bf
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-30 10:47:23 -0400

    initlizing the work experiance section

---
+++ components/MyExperiance.tsx
@@ -0,0 +1,13 @@
+const MyExperiance = () => {
+  return (
+    <section>
+      <div className="min-h-screen mt-[5rem]">
+        <h1 className="body_header">
+          My work <span className="gradient_color">Experiance</span>
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default MyExperiance;
