2f7cb5d9934f0266d9af8f4c9a595bcbfc5f5ace
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-30 16:53:04 -0400

    changed section id

--- components/MyExperiance.tsx
+++ components/MyExperiance.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const MyExperiance = () => {
   return (
-    <section id="projects" className="mt-10">
+    <section id="experiance" className="mt-10">
       <h1 className="body_header">
         My work <span className="gradient_color">Experiance</span>
       </h1>
