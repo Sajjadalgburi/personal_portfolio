96d19048861ae25929950742155b36d9ff9ca494
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-01 16:28:43 -0400

    initilizing the my approach section

---
+++ components/Approach.tsx
@@ -0,0 +1,12 @@
+const Approach = () => {
+  return (
+    <section id="approach" className=" mt-10">
+      <h1 className="body_header">
+        My <span className="gradient_color">Approach</span>
+      </h1>
+      <div>adw</div>
+    </section>
+  );
+};
+
+export default Approach;
