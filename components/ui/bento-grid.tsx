a71c13767b5a7460ac33e0f93a45194273451920
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-02 12:04:56 -0400

    improving the layout of the grid although it is not finished

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export const BentoGrid = ({
   className,
@@ -21,42 +23,74 @@ export const BentoGrid = ({
 
 export const BentoGridItem = ({
   className,
+  id,
   title,
   description,
-  header,
-  spareImg,
-  id,
-  titleClassName,
-  imgClassName,
   img,
-  icon,
+  imgClassName,
+  titleClassName,
+  spareImg,
 }: {
   className?: string;
-  title?: string | React.ReactNode;
-  description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  spareImg?: string;
-  id?: number;
+  id: number;
+  title?: string;
+  description?: string;
   img?: string;
-  titleClassName?: string;
   imgClassName?: string;
-  icon?: React.ReactNode;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
   return (
     <div
       className={cn(
-        "rounded-xl  group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
+      style={{
+        background: "rgb(25,25,25)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(24,24,24,0.7671860980720413) 0%, rgba(70,70,70,1) 100%)",
+      }}
     >
-      {header}
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
+        {/* Img Divs */}
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center ")}
+            />
+          )}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
+        <div
+          className={`absolute right-0 -bottom-5 ${
+            id === 5 && "w-full opacity-80"
+          } `}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              //   width={220}
+              className="object-cover object-center w-full h-full"
+            />
+          )}
+        </div>
+        {/*  */}
+
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
+        >
+          {" "}
+          <div className={titleClassName}>
+            <h1>{title}</h1>
+            <p>{description}</p>
+          </div>
         </div>
       </div>
     </div>
