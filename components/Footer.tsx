f1578820d0a0b6fd34552faca4dbea782c6b60d3
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-02 19:14:55 -0400

    fixing the responsiveness of the footer section to work with smaller screen sizes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,10 @@ import { footerData } from "@/data";
 
 const Footer = () => {
   return (
-    <footer id="contact" className="relative sm:mb-[20rem] pb-20 pt-36 h-96">
+    <footer
+      id="contact"
+      className="relative mb-[16rem] sm:mb-[20rem] pb-20 pt-36 h-96"
+    >
       <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.09] bg-grid-black/[0.2] absolute -top-[40%] left-0 flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_4%,black)]"></div>
       </div>
@@ -18,8 +21,8 @@ const Footer = () => {
           </p>
 
           <HoverBorderGradient>Learn More</HoverBorderGradient>
-          <div className="flex justify-between items-center w-full px-4 sm:mt-[12rem] mb-10">
-            <p className=" text-left text-xs font-medium">
+          <div className="sm:flex-row flex-col gap-8 flex justify-between items-center w-full px-4 sm:mt-[12rem] mt-[3rem] mb-4">
+            <p className="text-left text-xs font-medium">
               Copyright © 2024 Sajjad Alguri
             </p>
             <SocialLinks />
