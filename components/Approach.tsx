5305f9c1fa632c4423eb7b337ec497eb22765fbe
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-02 16:00:38 -0400

    passing differant colors that the original cards

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -19,6 +19,7 @@ const Approach = () => {
           >
             <CanvasRevealEffect
               animationSpeed={4}
+              colors={item.colors}
               containerClassName={item.containerClassName}
             />
           </Card>
