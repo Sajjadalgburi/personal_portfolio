0a253ca037e9d4d8f4f2edb144098fc346ac6311
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-03 12:12:24 -0400

    removing un-wanted imports

--- components/GirdGLobe.tsx
+++ components/GirdGLobe.tsx
@@ -1,10 +1,8 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
 
 const World = dynamic(
-  () => import("@/components/ui/globe").then((m) => m.World),
+  () => import("@/components/ui/Globe").then((m) => m.World),
   {
     ssr: false,
   }
