a44cc8d815e757594af072bb04da092bf9478f25
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-12 18:15:22 -0600

    refactor ResumePage component: add useEffect for document title and clean up structure

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,21 +1,33 @@
-const ResumePage = () => (
-  <div style={{ height: "100vh", width: "100%" }}>
-    <object
-      data="/resume/Sajjad_Algburi.pdf"
-      type="application/pdf"
-      width="100%"
-      height="100%">
-      <p>
-        Your browser does not support PDFs.
-        <a
-          href="/resume/Sajjad_Algburi.pdf"
-          target="_blank"
-          rel="noopener noreferrer">
-          Download the resume instead.
-        </a>
-      </p>
-    </object>
-  </div>
-);
+"use client";
+
+import { useEffect } from "react";
+
+const ResumePage = () => {
+  useEffect(() => {
+    document.title = "Sajjad Algburi | Resume";
+  }, []);
+
+  return (
+    <>
+      <div style={{ height: "100vh", width: "100%" }}>
+        <object
+          data="/resume/Sajjad_Algburi.pdf"
+          type="application/pdf"
+          width="100%"
+          height="100%">
+          <p>
+            Your browser does not support PDFs.
+            <a
+              href="/resume/Sajjad_Algburi.pdf"
+              target="_blank"
+              rel="noopener noreferrer">
+              Download the resume instead.
+            </a>
+          </p>
+        </object>
+      </div>
+    </>
+  );
+};
 
 export default ResumePage;
